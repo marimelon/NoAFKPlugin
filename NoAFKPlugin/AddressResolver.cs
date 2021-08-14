0dfda5cf9f066f3a2a5209f877d2bd6842a857c1
marimelon <29140293+marimelon@users.noreply.github.com>
2021-08-14 17:58:28 +0900

    init

---
+++ NoAFKPlugin/AddressResolver.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Game;
+using Dalamud.Game.Internal;
+using System;
+
+namespace NoAFKPlugin
+{
+    class AddressResolver : BaseAddressResolver
+    {
+        public IntPtr AFKTimeHandlerAddress { get; private set; }
+
+        public IntPtr CameraPotisionAddress { get; private set; }
+
+        protected override void Setup64Bit(SigScanner sig)
+        {
+            this.AFKTimeHandlerAddress = sig.ScanText("4c 8b dc 49 89 5b 18 49 89 73 20 55 57 41 54 41 55 41 57 49 8d 6b c8");
+        }
+    }
+}
---
+++ NoAFKPlugin/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using Dalamud.Hooking;
+using Dalamud.Plugin;
+using System.Runtime.InteropServices;
+
+namespace NoAFKPlugin
+{
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Timer
+    {
+        [FieldOffset(0x276c8)] public float AFKTimer1;   // kick timer?
+        [FieldOffset(0x276cc)] public float AFKTimer2;   // ???
+        [FieldOffset(0x276d0)] public float AutoLogoutTimer;
+        [FieldOffset(0x275a8)] public float AFKStatusLimit;
+    }
+
+    public class Plugin : IDalamudPlugin
+    {
+        public string Name => "NoAFK";
+
+        private DalamudPluginInterface pi;
+        private AddressResolver resolver;
+
+
+        private delegate void AfkTimerHandlerDelegate(long param_1, float param_2, long param_3);
+        private Hook<AfkTimerHandlerDelegate> AFKTimerHandlerHook;
+
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.pi = pluginInterface;
+
+            this.resolver = new AddressResolver();
+            this.resolver.Setup(this.pi.TargetModuleScanner);
+
+            this.AFKTimerHandlerHook = new(resolver.AFKTimeHandlerAddress, AfkTimerHandler);
+            this.AFKTimerHandlerHook.Enable();
+        }
+
+        public void Dispose()
+        {
+            this.AFKTimerHandlerHook.Dispose();
+            this.pi.Dispose();
+        }
+
+        private void AfkTimerHandler(long param_1, float param_2, long param_3)
+        {
+            this.AFKTimerHandlerHook.Original(param_1, 0.0f, param_3);
+        }
+    }
+}
